fce46e64b9eeb5630194db407c7288ad47d44584
Kratty <kizilirmak525@gmail.com>
2025-11-17 18:08:52 +0300

    fix: remove unused imports and clean up code structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,25 @@ import About from "./components/About/About";
 import MainPage from "./components/MainPage/MainPage";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Video from "./components/backgroundvideo/video";
 import QRGenerator from "./components/Portfolio/things/qr/QRGenerator";
 import Weather from "./components/Portfolio/things/weatherapp/Weather";
 
 function App() {
   return (
-    <Router>
+    <>
       <Video />
       <Navbar />
       <Routes>
-        <Route path="/krattydevpage" element={<MainPage />} />
-        <Route path="/krattydevpage/hakkimda" element={<About />} />
-        <Route path="/krattydevpage/portfolyo" element={<Portfolio />} />
-        <Route
-          path="/krattydevpage/portfolyo/qrgenerator"
-          element={<QRGenerator />}
-        />
-        <Route
-          path="/krattydevpage/portfolyo/weatherapp"
-          element={<Weather />}
-        />
-        <Route path="/krattydevpage/iletisim" element={<Contact />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="hakkimda" element={<About />} />
+        <Route path="portfolyo" element={<Portfolio />} />
+        <Route path="portfolyo/qrgenerator" element={<QRGenerator />} />
+        <Route path="portfolyo/weatherapp" element={<Weather />} />
+        <Route path="iletisim" element={<Contact />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
